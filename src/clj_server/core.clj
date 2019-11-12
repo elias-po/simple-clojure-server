4e0c9016b30f8382d1a3cd55aebc96456ed6d60c
elias-po <elias.popovs@gmail.com>
2019-11-13 00:10:28 +0200

    basic jetty server set up

--- src/clj_server/core.clj
+++ src/clj_server/core.clj
@@ -1,7 +1,9 @@
-(ns rtu-web.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(ns clj_server.core
+  (:require [ring.adapter.jetty :as jetty]))
+(defn ptag [content] (str "<p>" content "</p>"))
+(defn handler [request]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (str (ptag "String one")
+                 (ptag "String two"))})
+(defn -main [] (jetty/run-jetty handler {:port 3000}))
