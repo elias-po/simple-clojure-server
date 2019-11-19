7e411ca8d4ba7d214b4142512ea0e493b25e2177
elias-po <elias.popovs@gmail.com>
2019-11-19 16:51:32 +0200

    add ring plugin

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.7.1"]
+                 [ring/ring "1.7.1"]
                  [ring/ring-jetty-adapter "1.7.1"]]
+  :ring {:handler clj_server.core/handler}
+  :plugins [[lein-ring "0.12.5"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
