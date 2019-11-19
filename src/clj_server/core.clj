a7c7d177c1c1ac32dbc213c0baaca02d2fb1c604
elias-po <elias.popovs@gmail.com>
2019-11-19 18:27:52 +0200

    work-in-progress | parse vector to html

--- src/clj_server/core.clj
+++ src/clj_server/core.clj
@@ -1,9 +1,26 @@
 (ns clj_server.core
   (:require [ring.adapter.jetty :as jetty]))
-(defn ptag [content] (str "<p>" content "</p>"))
+(def input [
+              [:b "test phrase"]
+              [:hr]
+            ])
+(defn open-tag [tag-content]
+    (if (not= (second tag-content) nil)
+      (let [first (first tag-content)]
+        (str "<" first ">")
+        (open-tag (second tag-content))
+        (str "</" first ">")
+      )
+      tag-content
+    )
+)
+(defn gen-html [content]
+  (let [pointless "variable"]
+    (str (map open-tag content))
+))
 (defn handler [request]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    (str (ptag "String one")
-                 (ptag "String two"))})
+;   :body    (str (:uri request) '? (:query-string request))})
+   :body    (gen-html input)})
 (defn -main [] (jetty/run-jetty handler {:port 3000}))
