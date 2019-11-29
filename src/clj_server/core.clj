3ffef30961e0c03383b408767b0917953f75ecc0
elias-po <elias.popovs@gmail.com>
2019-11-29 22:55:39 +0200

    nit

--- src/clj_server/core.clj
+++ src/clj_server/core.clj
@@ -1,26 +1,27 @@
 (ns clj_server.core
   (:require [ring.adapter.jetty :as jetty]))
 (def input [
-              [:b "test phrase"]
-              [:hr]
+            [:hr]
+            [:b "test phrase"]
+            [:p [:a "test link"]]
             ])
 (defn open-tag [tag-content]
-    (if (not= (second tag-content) nil)
-      (let [first (first tag-content)]
-        (str "<" first ">")
-        (open-tag (second tag-content))
-        (str "</" first ">")
+  (if (not= (second tag-content) nil)
+    (let [first (first tag-content)]
+      (str "<" first ">")
+      (open-tag (second tag-content))
+      (str "</" first ">")
       )
-      tag-content
+    tag-content
     )
-)
+  )
 (defn gen-html [content]
   (let [pointless "variable"]
     (str (map open-tag content))
-))
+    ))
 (defn handler [request]
   {:status  200
    :headers {"Content-Type" "text/html"}
-;   :body    (str (:uri request) '? (:query-string request))})
+   ;   :body    (str (:uri request) '? (:query-string request))})
    :body    (gen-html input)})
 (defn -main [] (jetty/run-jetty handler {:port 3000}))
